42fa52d294c7c204347f33a795f35ec4cdd8c9bc
J2mF <mp4-12cs5@hotmail.com>
2022-12-16 18:31:05 +0300

    feat: add cart model

---
+++ lib/providers/cart.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem({
+    required this.id,
+    required this.title,
+    required this.quantity,
+    required this.price,
+  });
+
+  double get totalPrice => price * quantity;
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items => {..._items};
+
+  void addItem(String productId, double price, String title) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+        productId,
+        (value) => CartItem(
+          id: value.id,
+          title: value.title,
+          quantity: value.quantity + 1,
+          price: value.price,
+        ),
+      );
+      return;
+    }
+    _items.putIfAbsent(
+      productId,
+      () => CartItem(
+        id: DateTime.now().toString(),
+        title: title,
+        quantity: 1,
+        price: price,
+      ),
+    );
+  }
+}
