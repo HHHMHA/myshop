0c0a13a6b642ce3c51903839d2d26c06abc1f51f
J2mF <mp4-12cs5@hotmail.com>
2023-01-20 16:40:23 +0300

    feat(orders): implement orders provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myshop/providers/orders.dart';
 import 'package:myshop/screens/cart_screen.dart';
 import 'package:myshop/screens/product_detail_screen.dart';
 import 'package:provider/provider.dart';
@@ -19,6 +20,9 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider(
           create: (ctx) => Cart(),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => Orders(),
         )
       ],
       child: MaterialApp(
