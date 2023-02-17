431c48aefc1a66bdd5ec50758c6855a53a9b3c0e
J2mF <mp4-12cs5@hotmail.com>
2023-02-17 18:34:41 +0300

    feat(admin): implement edit product screen file

---
+++ lib/screens/edit_product_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class EditProductScreen extends StatefulWidget {
+  const EditProductScreen({Key? key}) : super(key: key);
+
+  @override
+  State<EditProductScreen> createState() => _EditProductScreenState();
+}
+
+class _EditProductScreenState extends State<EditProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Product'),
+      ),
+      body: Container(),
+    )
+  }
+}
